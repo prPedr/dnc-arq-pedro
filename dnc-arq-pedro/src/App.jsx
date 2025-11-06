b64f3407f028ed964e81919a99772ac021aa73b1
prPedr <prpedro025@gmail.com>
2025-11-06 05:11:56 -0300

    Configuracao das rotas da aplicacao

--- dnc-arq-pedro/src/App.jsx
+++ dnc-arq-pedro/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<><h1>HOME</h1></>}></Route>
+        <Route path="/about" element={<><h1>ABOUT</h1><Link to="/">Menu</Link></>}></Route>
+        <Route path="/projects" element={<><h1>PROJECTS</h1><Link to="/">Menu</Link></>}></Route>
+        <Route path="/contacts" element={<><h1>CONTACTS</h1><Link to="/">Menu</Link></>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
