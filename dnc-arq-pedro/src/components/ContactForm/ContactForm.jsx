45fdabfa11c71243cde919e122ebc2b8e28d075c
prPedr <prpedro025@gmail.com>
2025-11-11 04:34:00 -0300

    Incio do desenvolvimento da pagina de Contacts

---
+++ dnc-arq-pedro/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,18 @@
+// CSS
+import "./ContactForm.css"
+
+function ContactForm() {
+    return (
+        <div className="container">
+            <div className="contact-form d-flex fd-column al-center">
+                <h2>We love meeting new people and helping them.</h2>
+                <form>
+                    WIP...
+                </form>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ContactForm
--- dnc-arq-pedro/src/pages/Contact.jsx
+++ dnc-arq-pedro/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 // COMPONENTS
 import Header from "../components/Header/Header"
 import Banner from "../components/Banner/Banner"
+import ContactForm from "../components/ContactForm/ContactForm"
 import Footer from "../components/Footer/Footer"
 
 function Contatcts() {
@@ -9,6 +10,7 @@ function Contatcts() {
             <Header/>
             <Banner title="Contact Us" image="contact.svg"/>
             <div className="container">
+                <ContactForm/>
             </div>
             <Footer/>
         </>
