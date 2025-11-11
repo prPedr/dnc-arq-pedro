f77e213a5288d521b49f4fcc7241cc102ae716b8
prPedr <prpedro025@gmail.com>
2025-11-11 05:19:36 -0300

    Criando responsividade do Header utilizando o UseState

--- dnc-arq-pedro/src/components/Header/Header.jsx
+++ dnc-arq-pedro/src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 // CSS
@@ -6,14 +7,29 @@ import "./Header.css"
 // ASSETS
 import LogoDNC from "../../assets/dnc-logo.svg"
 
+// COMPONENTS
+import Button from "../Button/Button"
+
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+    
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"><img src={LogoDNC} alt="Logo da DNC" /></Link>
-
-                    <nav>
+                    <div className="menu-mobile">
+                        <Button buttonStyle="secondary" onClick={toggleMenu}>
+                            Menu
+                        </Button>
+                    </div>
+                    <nav className={`${isOpen ? "open" : ""}`}>
+                        <Button buttonStyle="unstyled" className="menu-mobile close-button" onClick={toggleMenu}>
+                            X
+                        </Button>
                         <ul className="d-flex">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/about">About us</Link></li>
@@ -22,8 +38,6 @@ function Header() {
                         </ul>
                     </nav>
                 </div>
-                
-                
             </div>
         </header>
     )
