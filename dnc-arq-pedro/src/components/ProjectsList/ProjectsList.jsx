6ce539cfffe91462ee197fe14d540843fdf56ccc
prPedr <prpedro025@gmail.com>
2025-11-13 04:58:11 -0300

    Consumo da API no ProjectList finalizado (As imagens nao estao funcionando por conta da API)

--- dnc-arq-pedro/src/components/ProjectsList/ProjectsList.jsx
+++ dnc-arq-pedro/src/components/ProjectsList/ProjectsList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 // CSS
 import "./ProjectsList.css"
 
@@ -5,7 +7,25 @@ import "./ProjectsList.css"
 import BotaoLikeVazio from "../../assets/like-vazio.svg"
 import BotaoLikeCurtiu from "../../assets/like-curtiu.svg"
 
+// UTILS
+import { getApiData } from "../../services/apiServices"
+
 function ProjectsList() {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const projectsResponse = await getApiData('projects')
+                setProjects(projectsResponse)
+            } catch {
+                setProjects([])
+            }
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div className="projects-section">
             <div className="projects-hero">
@@ -17,61 +37,16 @@ function ProjectsList() {
                 </p>
             </div>
             <div className="projects-grid">
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeVazio} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeVazio} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeCurtiu} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeVazio} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeVazio} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeVazio} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeCurtiu} alt="Botao de gostei do projeto" height="20px" />
-                </div>
-
-                <div className="projects-cards d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>Belo Horizonte, Brasil</p>    
-                    <img src={BotaoLikeVazio} alt="Botao de gostei do projeto" height="20px" />
-                </div>
+                {
+                    projects.map((project) => (
+                        <div className="projects-cards d-flex jc-center al-center fd-column" key={project.id}>
+                            <div className="thumb tertiary-background" style={{ backgroundImage: `url(${project.thumb})`}}></div>
+                            <h3>{project.title}</h3>
+                            <p>{project.subtitle}</p>
+                            <img src={BotaoLikeVazio} alt="Botão de gostei do projeto" height="20px" />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
