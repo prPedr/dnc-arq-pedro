50bbfa11717aa0749b8e24f40a199017cc18f7ea
prPedr <prpedro025@gmail.com>
2025-11-15 12:44:21 -0300

    Validacao de formulario com Hooks

--- dnc-arq-pedro/src/components/ContactForm/ContactForm.jsx
+++ dnc-arq-pedro/src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, use } from "react"
+
 // CSS
 import "./ContactForm.css"
 
@@ -5,6 +7,45 @@ import "./ContactForm.css"
 import Button from "../Button/Button"
 
 function ContactForm() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+    const [isFormValid, setIsFormValid] = useState(false)
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+        if (isFormValid) {
+            null
+        }
+    }
+
+    useEffect(() => {
+        const isValidEmail = (email) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            return emailRegex.test(email)
+        }
+
+        const isValid = formData.name.trim() && 
+        formData.email.trim() && 
+        isValidEmail(formData.email) && 
+        formData.message.trim() && 
+        formData.message.trim()
+
+        setIsFormValid(isValid)
+    }, [formData])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
     return (
         <div className="contact-form d-flex fd-column al-center">
             <h2>We love meeting new people and helping them.</h2>
@@ -16,6 +57,7 @@ function ContactForm() {
                         id="name"
                         name="name"
                         placeholder="Name *"
+                        onChange={handleChange}
                     />
 
                     <input
@@ -24,6 +66,7 @@ function ContactForm() {
                         id="email"
                         name="email"
                         placeholder="Email *"
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="form-group d-flex">
@@ -32,11 +75,12 @@ function ContactForm() {
                         name="message"
                         id="message"
                         placeholder="Mensagem *"
+                        onChange={handleChange}
                         rows="4">
                     </textarea>
                 </div>
                 <div className="al-center form-group jc-end d-flex">
-                    <Button type="submit" buttonStyle="secondary">Enviar</Button>
+                    <Button type="submit" buttonStyle="secondary" disabled={!isFormValid}>Enviar</Button>
                 </div>
             </form>
         </div>
