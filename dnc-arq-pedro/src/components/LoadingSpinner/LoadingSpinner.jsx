2af7144516724f0a5da2674930e6b77cfb022604
prPedr <prpedro025@gmail.com>
2025-11-16 19:59:42 -0300

    Incio do da personalizacao do loading

---
+++ dnc-arq-pedro/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+// CSS
+import "./LoadingSpinner.css"
+
+function LoadingSpinner() {
+    return (
+        <div className="d-flex al-center jc-center loading-overlay-container">
+            
+        </div>
+    )
+}
+
+export default LoadingSpinner
