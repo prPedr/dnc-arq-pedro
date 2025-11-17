44085c2d92f3d55c2f17117074aa80b8c52b531c
prPedr <prpedro025@gmail.com>
2025-11-17 05:15:12 -0300

    Utilizando Local Storage para armazenar o idioma salvo na secao anterior

--- dnc-arq-pedro/src/contexts/AppProvider.jsx
+++ dnc-arq-pedro/src/contexts/AppProvider.jsx
@@ -3,7 +3,8 @@ import { AppContext } from "./AppContext"
 import { getApiData } from "../services/apiServices.js"
 
 export const AppProvider = ({ children }) => {
-    const [language, setLanguage] = useState("br")
+    const savedLanguage = localStorage.getItem("lang")
+    const [language, setLanguage] = useState(savedLanguage ?? "br")
     const [languages, setLanguages] = useState()
     const [loading, setLoading] = useState(true)
 
@@ -20,6 +21,10 @@ export const AppProvider = ({ children }) => {
         }
         fetchLanguages()
     }, [])
+
+    useEffect(() => {
+        localStorage.setItem("lang", language)
+    }, [language])
 
     return (
         <AppContext.Provider value={{ language, languages, setLanguage, loading }}>
